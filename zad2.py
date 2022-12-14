b8de469cecc3eefb3aa83cb007dd5ba44895006f
Czarek <keracz1997@gmail.com>
2022-12-14 18:42:24 +0100

    zad2

--- zad2.py
+++ zad2.py
@@ -10,19 +10,20 @@ def thread(e):
 	# print("dupa: " + str(e))
 
 
-
-
 if __name__ == '__main__':
-	threads = list()
+
 	for index in range(4):
 		t = threading.Thread(target=thread, args=(index,))
-		threads.append(t)
+		t.name = index
+		print(t.name)
 		t.start()
-		print('Waiting for the thread...')
 		# t.join()
 
-	for index, thread in enumerate(threads):
-		print(f"Main: before joining {thread} index: {index}")
-		thread.join()
-		print(f"Main thread {index} done")
+	while 1:	
+		message = input(f"> ")
+
+	# for index, thread in enumerate(threads):
+	# 	print(f"Main: before joining {thread} index: {index}")
+	# 	thread.join()
+	# 	print(f"Main thread {index} done")
 
