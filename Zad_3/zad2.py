64a8e99d6ad60e7e0ad6e43ac107933859c2e727
Czarek Karczewski <keracz1997@gmail.com>
2023-01-26 19:23:19 +0100

    add whole zad2

--- Zad_3/zad2.py
+++ Zad_3/zad2.py
@@ -4,20 +4,40 @@ from time import sleep
 import string
 from tkinter import *
 
-# def controller_thread():
-# 	message = input(f"> ")
-# 	return message
+
+exit_event = threading.Event()
+def f(stop_event):
+    for letter in string.ascii_uppercase:
+        if not stop_event.is_set():
+            out = letter + str(threading.current_thread().name) + " "
+            text_output.insert(INSERT, out)
+            sleep(1)
+
+def start_thread(threads, index):
+    get_thread(threads, index).start()
+    
+def stop_thread(events, index):
+    events[int(index)-1].set()
+    
+def get_thread(threads, index):
+    for thread in threads:
+        if thread.name == index:
+            return thread
 
 def thread(e):
 	for letter in string.ascii_uppercase:
 		out = letter + str(e) + " "
 		text_output.insert(INSERT, out)
 		sleep(1)
-	# print("dupa: " + str(e))
 
-def thread_controller():
+def thread_starter():
 	input = text_input.get("1.0", "end-1c")
-	print(input)
+	start_thread(threads, input)
+	# print(input)
+ 
+def thread_stopper():
+    input = text_input.get("1.0", "end-1c")
+    stop_thread(events, input)
 
 if __name__ == '__main__':
 
@@ -30,32 +50,27 @@ if __name__ == '__main__':
 	text_output = Text(master, width=30, height=20)
 	text_output.grid(row=0, column=0, columnspan=3, sticky=EW)
 
-	start = Button(master, text="Start", width=5, command = lambda:thread_controller())
+	start = Button(master, text="Start", width=5, command = lambda:thread_starter())
 	start.grid(row=1, column=0, sticky=NW)
 
-	stop = Button(master, text="Stop", width=5)
+	stop = Button(master, text="Stop", width=5, command=lambda:thread_stopper())
 	stop.grid(row=1, column=0, sticky=SW)
 
 	text_input = Text(master, width=5, height=5)
 	text_input.grid(row=1, column=1, sticky=EW)
-	text_output.insert(INSERT, "Test")
+	# text_output.insert(INSERT, "Test")
 
 
-	
+	exit_flag = False
+	threads = []
+	events = []
+ 
 	for index in range(4):
-		t = threading.Thread(target=thread, args=(index,))
-		# t.name = index
-		# print(t.name)
-		t.start()
-		# t.join()
+		events.append(threading.Event())
+		thread = threading.Thread(target=f, name=index, args=[events[index-1]])
+		threads.append(thread)
+
 
 	master.mainloop()
-	# while 1:	
-	# 	t_controller = threading.Thread(target=controller_thread)
-	# 	t_controller.start()
 
-	# for index, thread in enumerate(threads):
-	# 	print(f"Main: before joining {thread} index: {index}")
-	# 	thread.join()
-	# 	print(f"Main thread {index} done")
 
