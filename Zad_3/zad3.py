420259174080b0397afefd40741fddd1f7a377a1
Czarek Karczewski <keracz1997@gmail.com>
2023-01-27 00:05:55 +0100

    zad3 changes

--- Zad_3/zad3.py
+++ Zad_3/zad3.py
@@ -6,13 +6,14 @@ from tkinter import *
 
 
 exit_event = threading.Event()
-def f(stop_event):
-    for letter in string.ascii_uppercase:
-        if not stop_event.is_set():
-            out = letter + str(threading.current_thread().name) + " "
-            text_output.insert(INSERT, out)
-            sleep(1)
+lock = threading.Lock()
 
+def work_thread():
+    with lock:
+    	for letter in string.ascii_uppercase:
+            print(letter + str(threading.current_thread().name) + " ")
+            sleep(1)
+         
 def start_thread(threads, index):
     get_thread(threads, index).start()
     
@@ -24,41 +25,8 @@ def get_thread(threads, index):
         if thread.name == index:
             return thread
 
-def thread(e):
-	for letter in string.ascii_uppercase:
-		out = letter + str(e) + " "
-		text_output.insert(INSERT, out)
-		sleep(1)
-
-def thread_starter():
-	input = text_input.get("1.0", "end-1c")
-	start_thread(threads, input)
-	# print(input)
- 
-def thread_stopper():
-    input = text_input.get("1.0", "end-1c")
-    stop_thread(events, input)
 
 if __name__ == '__main__':
-
-	master = Tk(className="Zad2") # main window
-	master.geometry("600x400")
-	master.columnconfigure(0, weight=1)
-	master.columnconfigure(1, weight=2)
-
-
-	text_output = Text(master, width=30, height=20)
-	text_output.grid(row=0, column=0, columnspan=3, sticky=EW)
-
-	start = Button(master, text="Start", width=5, command = lambda:thread_starter())
-	start.grid(row=1, column=0, sticky=NW)
-
-	stop = Button(master, text="Stop", width=5, command=lambda:thread_stopper())
-	stop.grid(row=1, column=0, sticky=SW)
-
-	text_input = Text(master, width=5, height=5)
-	text_input.grid(row=1, column=1, sticky=EW)
-	Label(master, text="Wpisz liczbę od 1-10 i naciśnij przycisk.").grid(row=2, column=1, sticky=EW)
 
 
 	exit_flag = False
@@ -67,10 +35,16 @@ if __name__ == '__main__':
  
 	for index in range(1, 11):
 		events.append(threading.Event())
-		thread = threading.Thread(target=f, name=index, args=[events[index-1]])
+		thread = threading.Thread(target=work_thread, name=index, args=[events[index-1]])
 		threads.append(thread)
+ 
+	for thread in threads:
+		thread.start()
+		# print(i)
+		# print(thr)
+		# start_thread(thr, i)
 
 
-	master.mainloop()
+
 
 
