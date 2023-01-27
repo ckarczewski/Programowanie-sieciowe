d5e5f303a6f93fcde589b60e4c048109358611ec
Czarek Karczewski <keracz1997@gmail.com>
2023-01-27 13:35:31 +0100

    finish zad3

--- Zad_3/zad3.py
+++ Zad_3/zad3.py
@@ -5,44 +5,27 @@ import string
 from tkinter import *
 
 
-exit_event = threading.Event()
 lock = threading.Lock()
 
-def work_thread():
+def work_thread(e):
     with lock:
     	for letter in string.ascii_uppercase:
-            print(letter + str(threading.current_thread().name) + " ")
+            print(letter + str(e) + " ")
             sleep(1)
          
-def start_thread(threads, index):
-    get_thread(threads, index).start()
-    
-def stop_thread(events, index):
-    events[int(index)-1].set()
-    
-def get_thread(threads, index):
-    for thread in threads:
-        if thread.name == index:
-            return thread
-
-
 if __name__ == '__main__':
 
 
-	exit_flag = False
 	threads = []
-	events = []
+
  
-	for index in range(1, 11):
-		events.append(threading.Event())
-		thread = threading.Thread(target=work_thread, name=index, args=[events[index-1]])
+	for index in range(1, 4):
+		thread = threading.Thread(target=work_thread, name=index, args=(index,))
 		threads.append(thread)
- 
+
 	for thread in threads:
 		thread.start()
-		# print(i)
-		# print(thr)
-		# start_thread(thr, i)
+		sleep(1)
 
 
 
