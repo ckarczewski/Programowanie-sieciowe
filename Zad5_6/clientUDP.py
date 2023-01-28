e52aec1a98da2afb9ee7fca7ad39e4386bb32c27
Czarek Karczewski <keracz1997@gmail.com>
2023-01-28 18:31:23 +0100

    client works but only when message is fitting in bufor

--- Zad5_6/clientUDP.py
+++ Zad5_6/clientUDP.py
@@ -12,24 +12,26 @@ def echo_client(port):
         print ("Error creating socket: %s" % e) 
         sys.exit(1) 
 
-    try:
-        sock.connect((HOST, port))
-    except socket.error as e:
-        print (f"Can not connect: {str(e)}")
-        sys.exit(1)
+    # try:
+    #     sock.connect((HOST, port))
+    # except socket.error as e:
+    #     print (f"Can not connect: {str(e)}")
+    #     sys.exit(1)
     
+    server_address = (HOST, port)
     while True: 
         message = input(f"> ")
         if message:
             try: 
                 # Send data 
+                sock.sendto(message.encode('utf-8'), server_address)
                 print (f"Sending: {message}") 
-                sock.sendall(message.encode('utf-8')) 
                 # Look for the response 
                 amount_received = 0 
                 amount_expected = len(message) 
                 while amount_received < amount_expected: 
-                    data = sock.recv(24)
+                    data, address = sock.recvfrom(24)
+                    print("GET Data from server")
                     if not data: 
                         print("Server disconnected, can not send message")
                         sock.close()
@@ -42,6 +44,7 @@ def echo_client(port):
             except Exception as e: 
                 print (f"Other exception: {str(e)}") 
                 break
+            print("powinno wyjsc z ifa")
             
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
