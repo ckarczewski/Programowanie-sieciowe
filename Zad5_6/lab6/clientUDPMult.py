40ee70084f1deb7c90b02b6318aa62ce567747b6
Czarek Karczewski <keracz1997@gmail.com>
2023-05-01 12:26:02 +0200

    add lab6 file

--- Zad5_6/lab6/clientUDPMult.py
+++ Zad5_6/lab6/clientUDPMult.py
@@ -5,46 +5,41 @@ import sys
 myHostName = socket.gethostname()
 HOST = socket.gethostbyname(myHostName)
 buffer_size = 36
-def echo_client(port):
+def echo_client(server_address):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
     except socket.error as e: 
         print ("Error creating socket: %s" % e) 
         sys.exit(1) 
 
-    # try:
-    #     sock.connect((HOST, port))
-    # except socket.error as e:
-    #     print (f"Can not connect: {str(e)}")
-    #     sys.exit(1)
+
     
-    server_address = (HOST, port)
+    # server_address = (HOST, port)
+    print(server_address)
     data=""
     while True: 
         message = input(f"> ")
         if message:
             try: 
                 message_parts = [message[i:i+buffer_size] for i in range(0, len(message), buffer_size)]
-                print(message_parts)
                 # Send data 
                 for part in message_parts:
                     sock.sendto(part.encode('utf-8'), server_address)
-                    print(f"Send part: {part}")
                 print (f"Sending: {message}") 
                 # Look for the response 
-                amount_received = 0 
-                amount_expected = len(message) 
-                while amount_received < amount_expected: 
-                    # data, address = sock.recvfrom(buffer_size)   
-                    data_temp, address = sock.recvfrom(buffer_size)
-                    print("data temp ", data_temp)
-                    data += str(data_temp)
-                    print("data ", data)
-                    if not data: 
-                        print("Server disconnected, can not send message")
-                        sock.close()
-                        break
-                    amount_received += len(data) 
+                # amount_received = 0 
+                # amount_expected = len(message) 
+                # while amount_received < amount_expected: 
+                #     # data, address = sock.recvfrom(buffer_size)   
+                #     data_temp, address = sock.recvfrom(buffer_size)
+                #     data += str(data_temp)
+                data, address = sock.recvfrom(buffer_size)
+                if not data: 
+                    print("Server disconnected, can not send message")
+                    sock.close()
+                    break
+                    # amount_received += len(data) 
                 print("GET Data from server")
                 print (f"Received: {data}") 
             except socket.error as e: 
@@ -62,5 +57,9 @@ if __name__ == '__main__':
     #     '--port', action="store", dest="port", type=int, required=True) 
     # given_args = parser.parse_args()  
     # port = given_args.port 
-    port = 6699
-    echo_client(port)
+    multicast_group = input("Wprowadź adres IP grupy multicast: ")
+    server_address = (multicast_group, int(input("Wprowadź numer portu: ")))
+
+    # port = 6699
+    # echo_client(port)
+    echo_client(server_address)
--- Zad5_6/lab6/serverUDPMult.py
+++ Zad5_6/lab6/serverUDPMult.py
@@ -1,20 +1,30 @@
 import socket
 import argparse 
 import sys
+import struct
 
 def echo_server(port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     except socket.error as e: 
         print ("Error creating socket: %s" % e) 
         sys.exit(1) 
 
     try:
         sock.bind(("0.0.0.0", port))
+        
     except socket.error as e: 
         print ("Error creating socket: %s" % e) 
         sys.exit(1) 
+        
+    mreq = struct.pack("4sl", socket.inet_aton("0.0.0.0"), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    return sock
 
+
+
+def receive(sock):
     while True:
         print("Waiting for message")
         data, address = sock.recvfrom(48)
@@ -29,7 +39,6 @@ def echo_server(port):
             print(f"Message: {data}")
             sock.sendto(data, address)
             print(f"Send data back to {address}")
-            
 if __name__ == '__main__':
     while True:
         command = input('Enter a command (start, stop)')
@@ -37,7 +46,8 @@ if __name__ == '__main__':
             port = input('Enter a port ')
             port = int(port)
             print('Starting the server')
-            echo_server(port)
+            addr = echo_server(port)
+            receive(addr)
         if command == 'stop':
             echo_server.close()
             print('Server stopped')
