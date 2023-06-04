2173a135affd346e7ec4045cdede943f6fa4c17e
Czarek Karczewski <keracz1997@gmail.com>
2023-06-04 13:57:23 +0200

    before i read script again, everything shoud work fine

--- SpeedTester/serverST.py
+++ SpeedTester/serverST.py
@@ -3,6 +3,8 @@ import argparse
 import sys
 import threading
 
+global socket_count
+
 # class Server(self, port):
 #     def __init__(self, port):
 #         self.port = port
@@ -10,40 +12,52 @@ import threading
 # Listen Thread
 def tcp_listener(sock):
     global client_socket, address
+    global client_thread
     try:
         sock.listen()
     except socket.error as e:
         print ("Listener error: %s" % e) 
-    while True:   
-        client_socket, address = sock.accept()
+    while True:
+        try:   
+            client_socket, address = sock.accept()
+        except socket.error as e:
+            print("cant accept")  
+        print(f"Connected with IP address: {address} ")
+        client_thread = threading.Thread(target=tcp_client(client_socket,address))
+        client_thread.start()
     # return client_socket, address
 
 # Client Thread
 def tcp_client(client_socket, address):
-    data = client_socket.recv(16)
-    if not data:
-        print("Client disconnected")
-    if data == "End":
-        print ("Closing connection to the server") 
-        client_socket.close()
-    elif data:
-        print(f"Message: {data}")
-        client_socket.send(data)
-        print(f"Send data back to {address}")
+    print("Ready to get message")
+    while True:
+        data = client_socket.recv(16)
+        if not data:
+            print("Client disconnected")
+            client_socket.close()
+            break
+        if data == "End":
+            print ("Closing connection to the server") 
+            client_socket.close()
+            break
+        elif data:
+            print(f"Message: {data}")
+            client_socket.send(data)
+            print(f"Send data back to {address}")
     
-def tcp_server(port):
+# def tcp_server(port):
     
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as e: 
-        print ("Error creating socket: %s" % e) 
-        sys.exit(1) 
+#     try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     except socket.error as e: 
+#         print ("Error creating socket: %s" % e) 
+#         sys.exit(1) 
         
-    try:
-        sock.bind(("0.0.0.0", port))
-    except socket.error as e: 
-        print ("Error creating socket: %s" % e) 
-        sys.exit(1) 
+#     try:
+#         sock.bind(("0.0.0.0", port))
+#     except socket.error as e: 
+#         print ("Error creating socket: %s" % e) 
+#         sys.exit(1) 
     
 
     
@@ -54,10 +68,21 @@ if __name__ == '__main__':
         if command == 'start':
             port = input('Enter a port ')
             port = int(port)
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            except socket.error as e: 
+                print ("Error creating socket: %s" % e) 
+                sys.exit(1)
+            try:
+                sock.bind(("0.0.0.0", port))
+            except socket.error as e: 
+                print ("Error creating socket: %s" % e) 
+                sys.exit(1)
             print('Starting the server')
-            tcp_server(port)
-            listen_thread = threading.Thread(target=tcp_listener)
-            client_thread = threading.Thread(target=tcp_client)
+            # tcp_server(port)
+            listen_thread = threading.Thread(target=tcp_listener(sock))
+            # listen_thread.daemon = True
+            listen_thread.start()
         if command == 'stop':
             tcp_server.close()
             print('Server stopped')
